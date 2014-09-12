2ce9f76e546d9fdec249670afb3542b746974f8d
Bernard Tison <bernard.tison@gmail.com>
2014-09-12 10:40:21 +0200

    specifiying server profile

--- bpms-quartz/bpms/configure-bpms.sh
+++ bpms-quartz/bpms/configure-bpms.sh
@@ -11,7 +11,7 @@ chown -R jboss:jboss $SERVER_INSTALL_DIR
 
 # Configure the server
 echo "Configure the Server"
-su jboss -c "$SERVER_INSTALL_DIR/$SERVER_NAME/bin/standalone.sh --admin-only &"
+su jboss -c "$SERVER_INSTALL_DIR/$SERVER_NAME/bin/standalone.sh --admin-only -c $JBOSS_CONFIG &"
 sleep 15
 su jboss -c "$SERVER_INSTALL_DIR/$SERVER_NAME/bin/jboss-cli.sh -c --controller=$IP_ADDR:9999 --file=$CLI_BPMS"
 su jboss -c "$SERVER_INSTALL_DIR/$SERVER_NAME/bin/jboss-cli.sh -c --controller=$IP_ADDR:9999 \":shutdown\" "
