671f2ac68f6fa8db096453e12ce38f9c646a73ae
Bernard Tison <bernard.tison@gmail.com>
2014-09-26 21:41:33 +0200

    RTGOV image : Rollup Patch 2

--- fsw-rtgov/rtgov/install-rtgov.sh
+++ fsw-rtgov/rtgov/install-rtgov.sh
@@ -2,19 +2,21 @@
 
 . /env.sh
 
-#Sanity checks
-if [ -f /$FSW ];
+# Sanity checks
+if [ -f $FSW ];
 then
-echo "File $FSW found"
+  echo "File $FSW found"
 else
-echo "File $FSW does not exists. Please put it in the resources folder"
-exit 255
+  echo "File $FSW does not exists. Please put it in the resources folder"
+  exit 255
 fi
 
-if [ -d $SERVER_INSTALL_DIR/jboss-eap-6.1 ] || [ -d $SERVER_INSTALL_DIR/$SERVER_NAME ]
+if [ -f $FSW_PATCH_ZIP ];
 then
-  echo "Target directory already exists. Please remove it before installing FSW again."
-  exit 250
+  echo "File $FSW_PATCH_ZIP found"
+else
+  echo "File $FSW_PATCH_ZIP does not exist. Please put it in the resources folder"
+  exit 255
 fi
 
 if [ -f $MYSQL_DRIVER ];
@@ -23,6 +25,12 @@ then
 else
     echo "File $MYSQL_DRIVER not found. Please put it in the resources folder"
     exit 255
+fi
+
+if [ -d $SERVER_INSTALL_DIR/jboss-eap-6.1 ] || [ -d $SERVER_INSTALL_DIR/$SERVER_NAME ]
+then
+  echo "Target directory already exists. Please remove it before installing FSW again."
+  exit 250
 fi
 
 #Setup FSW configuration file
@@ -60,6 +68,36 @@ fi
 # Install missing modules
 echo "Installing missing modules"
 cp -r $RESOURCES_DIR/modules/* $SERVER_INSTALL_DIR/$SERVER_NAME/modules/system/layers/soa
+
+# Apply Rollup Patch 2
+echo "Apply Fuse Service Works 6.0.0 Rollup Patch 2"
+unzip $FSW_PATCH_ZIP -d $RESOURCES_DIR
+# Base Installation
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/modules/system/layers/base/org/jboss/as/ejb3/
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/modules/system/layers/base/org/jboss/as/web/main/
+unzip $FSW_PATCH_BASE -d $RESOURCES_DIR
+mv $RESOURCES_DIR/jboss-eap-6.1 $RESOURCES_DIR/$SERVER_NAME
+cp -r $RESOURCES_DIR/$SERVER_NAME $SERVER_INSTALL_DIR
+rm -rf $RESOURCES_DIR/$SERVER_NAME 
+# RTGOV
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/standalone/deployments/overlord-rtgov/gadget-web.war/WEB-INF/lib/commons-fileupload-1.2.2.jar
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/standalone/deployments/overlord-rtgov/gadget-web.war/WEB-INF/lib/xstream-1.4.3.jar
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/quickstarts/overlord/rtgov/activityclient/pom.xml
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/quickstarts/overlord/rtgov/ordermgmt/app/pom.xml
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/quickstarts/overlord/rtgov/ordermgmt/ip/pom.xml
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/quickstarts/overlord/rtgov/ordermgmt/pom.xml
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/quickstarts/overlord/rtgov/policy/async/pom.xml
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/quickstarts/overlord/rtgov/policy/pom.xml
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/quickstarts/overlord/rtgov/policy/sync/pom.xml
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/quickstarts/overlord/rtgov/pom.xml
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/quickstarts/overlord/rtgov/sla/epn/pom.xml
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/quickstarts/overlord/rtgov/sla/monitor/pom.xml
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/quickstarts/overlord/rtgov/sla/pom.xml
+rm -rf $SERVER_INSTALL_DIR/$SERVER_NAME/quickstarts/overlord/rtgov/sla/report/pom.xml
+unzip $FSW_PATCH_RTGOV -d $RESOURCES_DIR
+mv $RESOURCES_DIR/jboss-eap-6.1 $RESOURCES_DIR/$SERVER_NAME
+cp -r $RESOURCES_DIR/$SERVER_NAME $SERVER_INSTALL_DIR
+rm -rf $RESOURCES_DIR/$SERVER_NAME
 
 # change owner to user jboss
 echo "Change owner to user jboss"
