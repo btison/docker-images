d8f4405ee2617f300147daa77e980bb6f4e41479
Bernard Tison <bernard.tison@gmail.com>
2015-03-31 11:07:43 +0200

    setting nexus work directory to host directory

--- nexus/start-nexus.sh
+++ nexus/start-nexus.sh
@@ -5,6 +5,7 @@
 IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
 export NEXUS_APPLICATION_HOST=$IPADDR
 export NEXUS_APPLICATION_PORT=$NEXUS_PORT
+export NEXUS_WORK=/data/nexus
 
 echo "IPADDR = $IPADDR"
 echo "NEXUS_APPLICATION_HOST = $NEXUS_APPLICATION_HOST"
