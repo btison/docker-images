10875370d777a1e8698292146e19c8c1cb220e8a
Bernard Tison <bernard.tison@gmail.com>
2015-04-20 12:25:57 +0200

    removed copying of mysql env variable

--- fsw/fsw/start-container.sh
+++ fsw/fsw/start-container.sh
@@ -5,11 +5,5 @@ echo "Running the run_supervisor function."
 supervisord 
 }
 
-__run_script() {
-echo "Running scripts"
-echo MYSQL_HOST=${DB_PORT_3306_TCP_ADDR} >> /env.sh
-}
-
 # Call all functions
-__run_script
 __run_supervisor
