4e4dc41d48abf41d928f23bdabe03b18f7749341
Bernard Tison <bernard.tison@gmail.com>
2016-11-19 10:36:01 +0100

    mysql: overhaul

---
+++ mysql/mysql/bin/run-mysqld.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+source "${CONTAINER_SCRIPTS_PATH}/common.sh"
+
+# Process the MySQL configuration files
+echo 'Processing MySQL configuration files ...'
+envsubst < ${CONTAINER_SCRIPTS_PATH}/my.cnf.template > $MYSQL_DEFAULTS_FILE
+
+if [ ! -d "$MYSQL_DATADIR/mysql" ]; then
+  initialize_database "$@"
+  shutdown_local_mysql
+fi
+
+# Restart the MySQL server with public IP bindings
+unset_env_vars
+echo 'Running final exec -- Only MySQL server logs after this point'
+exec /usr/bin/mysqld_safe --defaults-file=$MYSQL_DEFAULTS_FILE "$@" 2>&1
--- mysql/start-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-__run_supervisor() {
-echo "Running the run_supervisor function."
-supervisord 
-}
-
-# Call all functions
-__run_supervisor
