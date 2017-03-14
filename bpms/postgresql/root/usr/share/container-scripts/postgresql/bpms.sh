be1688ea129cd12543dad40aec8ecd019606a74d
Bernard Tison <bernard.tison@gmail.com>
2017-03-13 21:50:52 -0400

    Postgresql image based on openshift postgresql 9.4 image

--- bpms/postgresql/root/usr/share/container-scripts/postgresql/bpms.sh
+++ bpms/postgresql/root/usr/share/container-scripts/postgresql/bpms.sh
@@ -1,6 +1,6 @@
 function execute_scripts() {
   local database=${1?missing argument}
-  psql -c "grant all privileges on database ${database} to ${DB_USER};"
+  psql -c "grant all privileges on database ${database} to ${POSTGRESQL_USER};"
   psql -d ${database} < ${CONTAINER_SCRIPTS_PATH}/sql/postgresql-jbpm-schema.sql
   psql -d ${database} < ${CONTAINER_SCRIPTS_PATH}/sql/postgres-dashbuilder-schema.sql
   psql -d ${database} < ${CONTAINER_SCRIPTS_PATH}/sql/quartz_tables_postgres.sql
